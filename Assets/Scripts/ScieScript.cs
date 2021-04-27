cf531924f916d74618f4a0c5c7045183a26f0f64
Imem Belcadi <belcadiimem01@gmail.com>
2021-04-27 00:45:50 -0400

    Script mouvement Scie fonctionnel

---
+++ Assets/Scripts/ScieScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScieScript : MonoBehaviour
+{
+    [SerializeField] public float _range;
+    [SerializeField] public float _speed;
+    [SerializeField] private float _distance;
+    private int _position;
+    private Transform _transformScie;
+    private bool _direction = false; 
+    private int _switch = 1;
+    // direction == true -> ca monte
+    // direction == false -> ca descend
+    void Start()
+    {
+        _transformScie = GetComponent<Transform>();
+        InvokeRepeating("MoveScie", 0.1f, _speed);
+    }
+
+ 
+    void Update()
+    {
+        
+    }
+
+    void MoveScie()
+    {
+        if (_direction)
+        {
+            _transformScie.Translate(Vector3.up * _range);
+        }
+
+        if (!_direction)
+        {
+            _transformScie.Translate(Vector3.down * _range);
+        }
+        
+        _position = _position + _switch;
+        
+        if (_position >= _distance || _position  <= 0)
+        {
+            _direction = (1 * _switch == 1);
+            _switch = _switch * -1;
+            Debug.Log("direction " + _direction);
+        }
+    } 
+}
